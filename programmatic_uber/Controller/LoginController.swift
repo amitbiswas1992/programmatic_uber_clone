938d0c838dea25ca2b2c177b54c8540a8e770cab
Amit Biswas <biswas1992@hotmail.com>
2020-02-16 01:06:20 -0500

    Added  login VC

---
+++ programmatic_uber/Controller/LoginController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginController.swift
+//  programmatic_uber
+//
+//  Created by Amit Biswas on 2/16/20.
+//  Copyright © 2020 Amit Biswas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+    
+}
