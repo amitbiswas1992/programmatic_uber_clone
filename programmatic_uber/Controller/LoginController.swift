dce9b65449a71ff1b0245e653f60bd76b89e35de
Amit Biswas <biswas1992@hotmail.com>
2020-02-16 02:37:37 -0500

    Added title label programmatically

--- programmatic_uber/Controller/LoginController.swift
+++ programmatic_uber/Controller/LoginController.swift
@@ -10,11 +10,34 @@ import Foundation
 import UIKit
 
 class LoginController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // MARK: - Properties
+    private let titleLabel : UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
         
-        view.backgroundColor = .blue
-    }
+        return label
+    }()
+    
+//
+//    MARK: - LifeCycle
+        override func viewDidLoad() {
+            super.viewDidLoad()
+    
+            view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 25/255)
+            view.addSubview(titleLabel)
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+    
+    
+        }
+    
+        override var preferredStatusBarStyle: UIStatusBarStyle {
+            return .lightContent
+        }
+    
+    
     
 }
